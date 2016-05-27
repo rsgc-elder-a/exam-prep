e589bb2427db24d84878d249040080777166df75
rsgc-elder-a <aelder@rsgc.on.ca>
2016-05-27 10:26:19 -0400

    Added the algorithm and test plans into my word document.

---
+++ Exam Prep Warmuup #2: Vote Count/Exam Prep Warmuup #2: Vote Count/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Exam Prep Warmuup #2: Vote Count
+//
+//  Created by Andrew Elder on 2016-05-27.
+//  Copyright © 2016 Andrew Elder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
